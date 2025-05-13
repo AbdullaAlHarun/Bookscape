ad0f09299fa9ac28ebe158bbdcc8f9a006087b35
AbdullaAlHarun <abdhar01276@stud.noroff.no>
2025-05-14 00:19:11 +0200

    style: update Load More button to match theme colors

--- src/services/venueService.js
+++ src/services/venueService.js
@@ -1,13 +1,15 @@
 const API_BASE = "https://v2.api.noroff.dev";
 
-export const getAllVenues = async () => {
-  const url = `${API_BASE}/holidaze/venues?_bookings=false&_owner=false`;
-
-  const res = await fetch(url);
+export const getAllVenues = async (page = 1, limit = 8) => {
+  const res = await fetch(
+    `${API_BASE}/holidaze/venues?limit=${limit}&page=${page}`
+  );
   if (!res.ok) {
     throw new Error("Unable to fetch venues");
   }
-
   const json = await res.json();
-  return json.data;
+  return {
+    venues: json.data,
+    totalCount: json.meta.totalCount,
+  };
 };
