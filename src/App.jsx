8d7afee96caff000043fc7cbf8b4c6e419df3d67
AbdullaAlHarun <abdhar01276@stud.noroff.no>
2025-05-14 21:13:45 +0200

    fet: Add protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,40 @@ import ManagerDashboard from './pages/manager/ManagerDashboard';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Home from './pages/public/Home';
+import VenueDetailPage from "./pages/venues/[id]";
+import ProtectedRoute from './routes/ProtectedRoute'; 
 import './index.css';
-import VenueDetailPage from "./pages/venues/[id]"; 
-
 
 function App() {
   return (
     <Routes>
-      {/* Public Auth Route without layout */}
+      {/* Public Routes (no layout) */}
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
 
-      {/* All other routes within layout */}
+      {/* Protected Layout Routes */}
       <Route path="/" element={<MainLayout />}>
         <Route index element={<Home />} />
-        <Route path="customer" element={<CustomerDashboard />} />
-        <Route path="manager" element={<ManagerDashboard />} />
         <Route path="home" element={<Home />} />
         <Route path="/venues/:id" element={<VenueDetailPage />} />
-        
+
+        {/* ✅ Protected routes */}
+        <Route
+          path="customer"
+          element={
+            <ProtectedRoute>
+              <CustomerDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="manager"
+          element={
+            <ProtectedRoute>
+              <ManagerDashboard />
+            </ProtectedRoute>
+          }
+        />
       </Route>
     </Routes>
   );
