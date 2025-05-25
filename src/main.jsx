8435532aeffd75817795fe78443f883ec4b93834
AbdullaAlHarun <abdhar01276@stud.noroff.no>
2025-05-25 17:44:27 +0200

    Remove leftover react-helmet-async import

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import './index.css';
 import { AuthProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
       <AuthProvider>
         <BrowserRouter>
           <App />
         </BrowserRouter>
       </AuthProvider>
-    </HelmetProvider>
   </React.StrictMode>
 );
